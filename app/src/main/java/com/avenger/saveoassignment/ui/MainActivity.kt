a4f6472aea640ea1fdcf9b2791e7358223712bf8
unknown <vinod568312@gmail.com>
2021-07-25 11:17:17 +0530

    first commit

---
+++ app/src/main/java/com/avenger/saveoassignment/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.avenger.saveoassignment.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.avenger.saveoassignment.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
